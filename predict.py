75c740aa821d3f216e4a6709c33527c93f9e2a72
Rahulkumar Choudhary <59869006+Rahulkumar0909@users.noreply.github.com>
2022-04-29 13:44:20 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,20 +1,24 @@
-import matplotlib.pyplot as plt
+# Loading all the necessary libraries
 import cv2
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
 from keras.optimizers import SGD, Adam
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from keras.utils import to_categorical
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
 
+
+# Read the MNIST dataset and convert the type using pandas.
 data = pd.read_csv(r"A_Z Handwritten Data.csv").astype('float32')
 
-# print(data.head(10))
 
+# Split and Reshape the data
 X = data.drop('0',axis = 1)
 y = data['0']
 
@@ -23,11 +27,12 @@ train_x, test_x, train_y, test_y = train_test_split(X, y, test_size = 0.2)
 train_x = np.reshape(train_x.values, (train_x.shape[0], 28,28))
 test_x = np.reshape(test_x.values, (test_x.shape[0], 28,28))
 
-print("Train data shape: ", train_x.shape)
-print("Test data shape: ", test_x.shape)
 
+# converting floating point value to integer value of the labels
 word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
 
+
+# Plotting the number of alphabets in the dataset
 y_int = np.int0(y)
 count = np.zeros(26, dtype='int')
 for i in y_int:
@@ -46,6 +51,7 @@ plt.grid()
 plt.show()
 
 
+# 
 shuff = shuffle(train_x[:100])
 
 fig, ax = plt.subplots(3,3, figsize = (10,10))
